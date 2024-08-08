0194077ba393dfc271db9f5742d6746a6c0ac23b
faisalgaur123 <118573543+faisalgaur123@users.noreply.github.com>
2024-08-08 18:01:16 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Services;
+
+internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        //int empID = 101;
+        //string empName = "Faisal";
+        //double empSalary = 10000.89;
+        //Console.WriteLine("Employe ID   " + empID);
+        //Console.WriteLine("Employe Name   " + empName);
+        //Console.WriteLine("Employe Salary   " + empSalary);
+
+
+
+
+        //int ID;
+        //string Name;
+        //float Salary;
+        //Console.WriteLine("Enter Your Employe ID :");
+        //ID=int.Parse(Console.ReadLine());
+        //Console.WriteLine("Enter Your Employe Name :");
+        //Name=Console.ReadLine();
+        //Console.WriteLine("Enter Your Employe Salary :");
+        //Salary =float.Parse(Console.ReadLine());
+        //Console.WriteLine($"Employe ID is: {ID}|| Employe Name is: {Name}|| Employe Salary is: {Salary}");
+
+
+        //Decision making
+        //int pin_no = 1234, upin;
+        //double ex_amount = 30000,amount,balance;
+        //Console.WriteLine("Enter your PIN");
+        //upin = int.Parse(Console.ReadLine());
+        //if (pin_no == upin)
+        //{
+        //    Console.WriteLine("Your Entered Pin Sucessfully");
+        //    Console.WriteLine("Enter Your Amount");
+        //    amount=double.Parse(Console.ReadLine());
+        //    if(amount <= ex_amount)
+        //    {
+        //        Console.WriteLine("Your Transuction Successfully Done ");
+        //        balance = ex_amount - amount;
+
+        //        Console.WriteLine($"Your Amount is {amount} your balance is {balance}");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Insufficient Fund !! Try Again");
+        //    }
+
+        //}
+        //else
+        //{
+        //    Console.WriteLine("You Entered Invalid Pin !! Try again");
+        //}
+
+
+
+        //else if conditional statment
+
+        //double basic_salary, pf_amount, net_salary;
+        //Console.WriteLine("Enter Your Salary");
+        //basic_salary=double.Parse(Console.ReadLine());
+        //if(basic_salary<10000)
+        //{
+        //    pf_amount = basic_salary * 0.02;
+        //    net_salary = basic_salary - pf_amount;
+        //    Console.WriteLine($"Your Basic Salry is {basic_salary} ,\n Your PF Amount is {pf_amount} ,\n  Your Net Salary:{net_salary}");
+        //}
+        //else if(basic_salary>10000 && basic_salary < 20000)
+        //{
+        //    pf_amount = basic_salary * 0.05;
+        //    net_salary = basic_salary - pf_amount;
+        //    Console.WriteLine($"Your Basic Salry is {basic_salary} ,\n Your PF Amount is {pf_amount} ,\n  Your Net Salary:{net_salary}");
+        //}
+        //else if (basic_salary > 20000 && basic_salary < 30000)
+        //{
+        //    pf_amount = basic_salary * 0.1;
+        //    net_salary = basic_salary - pf_amount;
+        //    Console.WriteLine($"Your Basic Salry is{basic_salary},\n Your PF Amount is{pf_amount},\n  Your Net Salary:{net_salary}");
+        //}
+        //else if (basic_salary > 30000 && basic_salary < 40000)
+        //{
+        //    pf_amount = basic_salary * 0.15;
+        //    net_salary = basic_salary - pf_amount;
+        //    Console.WriteLine($"Your Basic Salry is{basic_salary},\n Your PF Amount is{pf_amount},\n  Your Net Salary:{net_salary}");
+        //}
+        //else
+        //{
+        //    pf_amount = basic_salary * 0.2;
+        //    net_salary = basic_salary - pf_amount;
+
+        //    Console.WriteLine($"Your Basic Salry is{basic_salary},\n Your PF Amount is{pf_amount},\n  Your Net Salary:{net_salary}");
+        //}
+
+
+
+
+
+
+
+
+        //switch Case : Menu
+
+        //int choice;
+        //Console.WriteLine("Enter Your Choice \n 1.Deposite  \n 2.Check Balance \n 3.Withdraw \n 4.Exit ");
+        //choice = int.Parse(Console.ReadLine());
+        //switch (choice)
+        //{
+        //        case 1:
+        //        Console.WriteLine("Your Deposite section Execute");
+        //        break;
+        //        case 2:
+        //        Console.WriteLine("Your Bank Balance is 97980");
+        //        break;
+        //        case 3:
+        //        Console.WriteLine("Your Withdraw Amount is 8009"); 
+        //        break;
+        //        case 4:
+        //        Console.WriteLine("Do you Want to Exit");
+        //        break;
+
+        //    default:
+        //        Console.WriteLine("Invalid Opt Select");
+        //        break;
+        //}
+
+
+
+
+        //looping
+
+        //print 1 to 10 digits
+
+        //int i, sum = 0;
+        //for (i=1;i<=10;i++)
+        //{
+
+        //    sum=sum+i;
+        //}
+        //Console.WriteLine($"The Sum of First Natural No. {sum}");
+
+
+
+
+
+        //Tables
+
+        //int i,num;
+        //Console.WriteLine("Input The Numbers Of Terms");
+        //num = int.Parse(Console.ReadLine());
+        //for(i=1;i<=10; i++)
+        //{
+
+        //    Console.WriteLine($"The Table Of {num}*{i} :"+(num*i));
+        //}
+
+
+        // print odd naturals No and Their Sum
+        //int i, num,sum=0;
+        //Console.WriteLine("Enter the No.");
+        //num=int .Parse (Console.ReadLine());
+        //for(i=1; i<=num; i++)
+        //{
+        //    int odd = (2 * i - 1);
+        //    Console.WriteLine("The odd No is "+odd);
+        //    sum=sum+odd;
+        //}
+        //Console.WriteLine($"The sum of odd {num} natural Numbers Is :"+ sum);
+
+
+
+
+        //*****Star Pattern
+
+
+
+        //int i,j;
+
+        //for (i=1; i<10;i++)
+        //{
+        //    for(j=1; j <= i; j++)
+        //    {
+        //        Console.Write(j);
+        //    }
+        //    Console.WriteLine();
+
+        //}
+
+        //int a = 4;
+        //int b=5;
+        //Console.WriteLine($"The value of a is:{a} and b is {b} before swapping");
+
+        //a = a + b;
+        //b= a-b;
+        //a = a - b;
+        //Console.WriteLine($"The Value of a is: {a} and b is: {b} after swapping ");
+        //Console.ReadLine();
+
+
+
+
+        //Array
+
+        //int i;
+        //Console.WriteLine("For using For Loop:");
+        //int[] arr = { 1,2,3,4,5,6,7,8,9,10};
+        //for(i=0;i<arr.Length;i++)
+        //{
+        //    Console.Write(arr[i]+" ");
+
+        //}
+
+        //Console.WriteLine("For Using Foreach Loop:");
+
+
+        //foreach(int num in arr)
+        //{
+        //    Console.Write(num+" ");
+        //}
+
+
+
+       
+
+
+
+
+
+        }
+
+    }
+    
+
